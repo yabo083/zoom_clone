83d7dca77f73a61ffd10b38a270c76110cba865f
yabo083 <103875886+yabo083@users.noreply.github.com>
2024-05-03 22:04:33 +0800

    first commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,21 @@
+import StreamVideoProvider from "@/providers/StreamClientProvider";
+import { Metadata } from "next";
+import React, { Children, ReactNode } from "react";
+
+export const metadata: Metadata = {
+  title: "Yoom",
+  description: "Video calling app",
+  icons: {
+    icon: "/icons/logo.svg",
+  },
+};
+
+const RootLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
+  return (
+    <main>
+      <StreamVideoProvider>{children}</StreamVideoProvider>
+    </main>
+  );
+};
+
+export default RootLayout;
