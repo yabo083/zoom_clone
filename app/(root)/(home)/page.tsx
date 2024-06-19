6afb657cc4fee43315f879c2d777d028c1213c36
yabo083 <103875886+yabo083@users.noreply.github.com>
2024-06-19 20:14:49 +0800

    跨代理的时间显示

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,11 +1,10 @@
 import MeetingTypeList from "@/components/MeetingTypeList";
 
-
 const Home = () => {
   const now = new Date();
 
-  const time = now.toLocaleTimeString('en-US', { hour: "2-digit", minute: "2-digit", hour12: true});
-  const date = (new Intl.DateTimeFormat('en-US', {dateStyle: 'full'})).format(now);
+  const time = now.toLocaleTimeString(undefined, { hour: "2-digit", minute: "2-digit", hour12: true});
+  const date = new Intl.DateTimeFormat(undefined, { dateStyle: 'full' }).format(now);
 
   return (
     <section className="flex size-full flex-col gap-10 text-white">
